c7b070aa150c8c71f6778e7d4063a0e12ed9baad
Antonio Pelusi <antoniopelusi2000@gmail.com>
2025-07-18 11:26:44 +0200

    Update fetch target to obtain all repos

--- fetch.py
+++ fetch.py
@@ -2,46 +2,47 @@ import requests
 import json
 
 def fetch_repos():
-    url = "https://api.github.com/users/antoniopelusi/repos"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
+    base_url = "https://api.github.com/users/antoniopelusi/repos"
+    all_repos = []
+    page = 1
+    per_page = 100  # Maximum allowed by GitHub
+
+    while True:
+        url = f"{base_url}?per_page={per_page}&page={page}"
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print(f"Errore: Impossibile ottenere le repository (status code: {response.status_code})")
+            break
+
         repos = response.json()
-        filtered_repos = []
-        
+        if not repos:
+            break  # No more repos
+
         for repo in repos:
-            # Endpoint per ottenere i linguaggi della repository
             languages_url = repo["languages_url"]
             languages_response = requests.get(languages_url)
-            
-            # Controlla se la richiesta per i linguaggi è andata a buon fine
-            if languages_response.status_code == 200:
-                languages = languages_response.json()
-            else:
-                languages = {}
-            
-            # Creazione dell'oggetto filtro
+            languages = languages_response.json() if languages_response.status_code == 200 else {}
+
             filtered_repo = {
                 "created_at": repo["created_at"],
                 "name": repo["name"],
                 "html_url": repo["html_url"],
-                "languages": list(languages.keys())  # Solo i nomi dei linguaggi
+                "languages": list(languages.keys())
             }
-            filtered_repos.append(filtered_repo)
-        
-        # Salva i risultati in un file JSON
-        final_json = json.dumps(filtered_repos, indent=4)
-        with open('assets/data/repos.json', 'w') as json_file:
-            json_file.write(final_json)
-        
-        print("Repos: ready")
-    else:
-        print(f"Errore: Impossibile ottenere le repository (status code: {response.status_code})")
+            all_repos.append(filtered_repo)
+
+        page += 1  # Next page
+
+    final_json = json.dumps(all_repos, indent=4)
+    with open('assets/data/repos.json', 'w') as json_file:
+        json_file.write(final_json)
+    print("Repos: ready")
 
 def fetch_cv():
     url = "https://drive.google.com/uc?export=download&id=1gO0xD8AMeXDzXfcd_xIzL01kG7lrVOGj"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         with open("assets/data/cv.pdf", "wb") as file:
             file.write(response.content)
